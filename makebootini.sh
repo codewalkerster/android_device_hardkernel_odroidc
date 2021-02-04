1572b2376ac3232d43aab5b4fe9fab09404e4f57
Chris <codewalker@hardkernel.com>
2021-02-04 14:37:04 +0900

    ODROID-C1: Modify makebootini.sh.
    Change-Id: I4320919611ea4836b308e7ba6c0d735bfc960671

--- makebootini.sh
+++ makebootini.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
-if [ -f "/internal/boot.ini" ]
+if [ ! -f "/internal/boot.ini" ]
 then
-    break
-else
     cp /system/etc/boot.ini.template /internal/boot.ini
 fi
 
-if [ -f "/internal/usb_modeswitch.con" ]
+if [ ! -f "/internal/usb_modeswitch.con" ]
 then
-    break
-else
     cp /system/etc/usb_modeswitch.conf.template /internal/usb_modeswitch.conf
 fi
 
